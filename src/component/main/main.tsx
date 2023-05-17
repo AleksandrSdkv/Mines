6fd508d90663f5dca13476a764c5708514d9c381
unknown <Aleksandr2697@yandex.ru>
2023-05-17 23:24:28 +0300

    добавление мин

--- src/component/main/main.tsx
+++ src/component/main/main.tsx
@@ -2,12 +2,23 @@ import React from 'react';
 import Cell from './cell/cell';
 import './main.css'
 function Main() {
-
+    const Mine = -1;
     function createField(size: number): number[] {
         const field: number[] = new Array(size * size).fill(0);
-        return field
+        for (let i = 0; i < size;) {
+            const x = Math.floor(Math.random() * size);
+            const y = Math.floor(Math.random() * size);
 
+            if (field[y * size + x] === Mine) continue;
+
+            field[y * size + x] = Mine;
+
+            i += 1;
+        }
+
+        return field
     }
+    
     return (
         <main className='main'>
             <div className='main__container'>
