bdd57dbaf0a3535c8d2bb8e932b8bd9e2d294c73
unknown <Aleksandr2697@yandex.ru>
2023-05-19 00:01:12 +0300

    add mines

--- src/component/main/cell/cell.js
+++ src/component/main/cell/cell.js
@@ -1,10 +1,10 @@
 import './cell.css'
 function Cell(prop) {
-    console.log(prop)
-    return (
-        <li className='cell' >
 
-        </li>
+    return (
+        <button className='cell' >
+
+        </button>
     )
 }
 export default Cell;
--- src/component/main/main.tsx
+++ src/component/main/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cell from './cell/cell';
 import './main.css'
 function Main() {
     const Mine = -1;
+    const size = 16;
+    const dimension = new Array(size).fill(null)
+    const [field, setField] = useState<number[]>(() => createField(size))
     function createField(size: number): number[] {
         const field: number[] = new Array(size * size).fill(0);
+
+
+        function inc(x: number, y: number) {
+            if (x >= 0 && x < size && y >= 0 && y < size) {
+                if (field[y * size + x] === Mine) return;
+                field[y * size + x] += 1
+            }
+        }
+
         for (let i = 0; i < size;) {
             const x = Math.floor(Math.random() * size);
             const y = Math.floor(Math.random() * size);
@@ -14,11 +26,21 @@ function Main() {
             field[y * size + x] = Mine;
 
             i += 1;
+            inc(x + 1, y)
+            inc(x - 1, y)
+            inc(y - 1, x)
+            inc(y + 1, x)
+            inc(x + 1, y - 1)
+            inc(x - 1, y + 1)
+            inc(x - 1, y - 1)
+            inc(x + 1, y + 1)
         }
 
         return field
     }
-    
+
+
+
     return (
         <main className='main'>
             <div className='main__container'>
@@ -28,7 +50,14 @@ function Main() {
                     <div>score</div>
                 </div>
                 <ul className='main__cells'>
-
+                    {dimension.map((_, y) => {
+                        return (<div key={y}>
+                            {dimension.map((_, x) => {
+                                return (<div key={x} >{field[y * size + x]} </div>)
+                            })}
+                        </div>)
+                    })
+                    }
                 </ul>
             </div>
         </main>
